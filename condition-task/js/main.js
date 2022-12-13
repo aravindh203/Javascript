0da0c42ddaa145b82899701075c29f5d8d20653c
aravindh203 <aravindhkumar218@gmail.com>
2022-12-13 16:58:44 +0530

    add

---
+++ condition-task/js/main.js
@@ -0,0 +1,55 @@
+/*
+let a=prompt("enter the number");
+if(a>=0){
+    document.write("Given number is positive");
+}
+else{
+    document.write("Given number is negative");
+}
+
+
+let age=prompt("enter your age");
+if(age<16){
+    document.write("child");
+}
+else if(age<50){
+    document.write("adult");
+}
+else{
+    document.write("senior adult");
+}
+
+
+var x=parseInt(prompt("enter the number"));
+for(i=1;i<=x;i++){
+    if(i%3==0 && i%5==0){
+        document.write(i+"<br>");
+    }
+}
+
+
+let x=parseInt(prompt("enter the jercy number"))
+switch(x){
+    case 7:
+        document.write("Dhoni");
+        break;
+    case 18:
+        document.write("Kholi");
+        break;
+    case 45:
+        document.write("Rohith");
+        break;
+    case 17:
+        document.write("Devillers");
+        break;
+    case 8:
+        document.write("Jadeja");
+        break;
+} 
+*/
+let i=1;
+while(i<=5)
+{
+    document.write(i+"<br>")
+    i++;
+}
